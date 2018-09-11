b5f05e973d07b08d28865ce402ebe71a0149a3d0
Jordan Espejo <Jordan@Jordans-Mac-mini.local>
2018-09-10 22:41:33 -0700

    change link text

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const SecondPage = () => (
   <div>
     <h1>Hello from the other side</h1>
-    <p>Check out my other website <a href="https://xn--yckq0jzb2b.xn--tckwe">ジョーダン.コム</a> </p>
+    <p>Check out<a href="https://xn--yckq0jzb2b.xn--tckwe">my other website</a> </p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
