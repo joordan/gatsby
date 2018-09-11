448b2e357e4ac4ad29cc66f9e5c26ede14bdf5ad
Jordan Espejo <Jordan@Jordans-Mac-mini.local>
2018-09-10 22:37:36 -0700

    Added other website

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const SecondPage = () => (
   <div>
     <h1>Hello from the other side</h1>
-    <p>Welcome to page 2</p>
+    <p>Check out my other website <a href="https://xn--yckq0jzb2b.xn--tckwe">ジョーダン.コム</a> </p>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
